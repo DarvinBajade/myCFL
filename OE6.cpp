8b51e3157b3720a31ad3f4f481983ef52418bed5
DarvinBajade <bajade.darvinkobe@gmail.com>
2021-11-25 22:29:05 +0800

    OE6

---
+++ OE6.cpp
@@ -0,0 +1,141 @@
+//name: Bajade, Darvin Kobe M.
+//activity name: OE6
+//description: Array Programming
+//date: 11/24/2021
+
+#include<iostream>
+
+using namespace std;
+
+void unsorted(int a[], int b);
+void ascending(int a[], int b);
+void descending(int a[], int b);
+void Tryagain();
+int ans=1;
+
+const int size = 100;
+
+int main (){
+    int a[size], b, ch;
+    system("cls");
+    cout << "-----------------------------------" << endl;
+    cout << "Number of Elements in Array" << endl;
+    cout << "-----------------------------------" << endl;
+    cout << endl;
+    cout << "quantity of elements: ";
+    cin >> b;
+
+    system("cls");
+    cout << "-----------------------------------" << endl;
+    cout << "Enter " << b << " numbers" << endl;
+    cout << "-----------------------------------" << endl;
+      
+    for(int i = 0; i < b; i++){
+        cin >> a[i];
+    }
+
+    do{
+        system("cls");
+        cout << "-----------------------------------" << endl;
+        cout << " Array sorting Program" << endl;
+        cout << "-----------------------------------" << endl;
+        cout << " [1] Searching Array " << endl;
+        cout << " [2] Sort Array Ascending " << endl;
+        cout << " [3] Sort Array Descending " << endl;
+        cout << " [4] Exit " << endl;
+        cout << "-----------------------------------" << endl;
+        cin >> ch;
+
+        switch (ch){
+            case 1:{
+                unsorted(a,b);
+                Tryagain();
+                break;
+            }
+            case 2:{
+                ascending(a,b);
+                Tryagain();
+                break;
+            }
+            case 3:{
+                descending(a,b);
+                Tryagain();
+                break;
+            }
+            case 4:{
+                system("cls");
+                cout << "-----------------------------------" << endl;
+                cout << "Thank you for using the program" << endl;
+                cout << "-----------------------------------" << endl;
+                return 0;
+            }                
+            
+            default:
+                system("cls");
+                cout << "-----------------------------------" << endl;
+                cout << "Invalid Input" << endl;
+                cout << "-----------------------------------" << endl;
+                Tryagain();
+                break;
+        }
+    }while (ans==1);
+    
+}
+
+void unsorted(int a[], int b){
+    system("cls");
+    for(int x=0; x<b;x++){
+        cout << "Unsorted Array Elmement ["<<x<<"] "<< a[x] << endl;
+    }
+}
+
+void ascending(int a[], int b){
+    int j, y, temp;
+
+    for(y=0; y<b; y++){
+        for(j=y+1; j<b; j++){
+            if(a[j] < a[y]){
+                temp = a[y];
+                a[y] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    
+    system("cls");
+    cout<<"Elements of array in sorted ascending order:"<<endl;
+    for(y=0; y<b; y++){
+        cout<<"Ascending Array : "<<a[y]<<endl;
+    }
+}
+
+void descending(int a[], int b){
+    int j, y, temp;
+
+    for (y=0; y<b; ++y){
+        for (j=y+1;j<b;++j){
+            if (a[y] < a[j]){
+                temp = a[y];
+                a[y] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    system("cls");
+    cout<< endl << "Numbers in Descending Order : " << endl;
+    for (y = 0; y < b; ++y){
+        cout<<"Descending Array : "<<a[y];
+        cout<< endl;
+    }
+}
+
+void Tryagain(){
+    cout << "------------------------------------" << endl;
+    cout << "Do you want to try again?"<< endl;
+    cout << "1 = to continue" << endl;
+    cout << "2 = to exit" << endl;
+    cout << "------------------------------------"<< endl;
+    cin >> ans;
+    system("cls");
+}
